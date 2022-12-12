509060ffd5763b7c799aacf04d5a4d65ee24f592
yudiz_DARSHAN <darshan.bhensadadiya@yudiz.com>
2022-12-12 18:50:30 +0530

    grid card change

--- src/shared/Component/Blogs/blogs.jsx
+++ src/shared/Component/Blogs/blogs.jsx
@@ -9,17 +9,20 @@ function Blog() {
         <div className='blog'>
             <Container>
                 <h1 className='blog_title'>Blogs</h1>
-                <Row xs={1} md={4} className="g-4">
-                    <Col sm>
+                <Row xs={1} sm={2} md={3} lg={4} xl={5} className="g-4">
+                    <Col>
                         <BlogCart img='https://images.unsplash.com/photo-1559666126-84f389727b9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8ODV8MzQ0NDQ5NHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60' />
                     </Col>
-                    <Col sm>
+                    <Col>
                         <BlogCart img='https://images.unsplash.com/photo-1516166328576-82e16a127024?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MzJ8MzQ0NDQ5NHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60' />
                     </Col>
-                    <Col sm>
+                    <Col>
                         <BlogCart img='https://images.unsplash.com/photo-1575211447836-6b5943b34a03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NDB8MzQ0NDQ5NHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60' />
                     </Col>
-                    <Col sm>
+                    <Col>
+                        <BlogCart img='https://images.unsplash.com/photo-1494879540385-bc170b0878a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NDZ8MzQ0NDQ5NHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60' />
+                    </Col>
+                    <Col>
                         <BlogCart img='https://images.unsplash.com/photo-1494879540385-bc170b0878a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NDZ8MzQ0NDQ5NHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60' />
                     </Col>
 
